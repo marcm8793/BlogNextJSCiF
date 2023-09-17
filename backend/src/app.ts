7c20703bb934ba0ba1f16a63877475a59528198a
Marcosss87 <marcmansour@outlook.fr>
2023-09-17 16:46:14 +0200

    backend login

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,11 @@ import env from "./env";
 import morgan from "morgan";
 import errorHandler from "./middlewares/errorHandler";
 import createHttpError from "http-errors";
+import userRoutes from "./routes/user";
+import session from "express-session";
+import passport from "passport";
+import sessionConfig from "./config/session";
+import "./config/passport";
 
 const app = express();
 
@@ -19,8 +24,13 @@ app.use(
   })
 );
 
+app.use(session(sessionConfig));
+
+app.use(passport.authenticate("session"));
+
 app.use("/uploads/featured-images", express.static("uploads/featured-images"));
 
+app.use("/users", userRoutes);
 app.use("/posts", blogPostRoutes);
 
 app.use((req, res, next) => next(createHttpError(404, "Endpoint not found")));
